5b62293ff647298f87dd923bb1fb0e78a422cc0c
AEntropia <sandman2871@gmail.com>
2025-11-06 11:52:20 -0300

    fix: corrigir swagger e CORS para vercel

--- config/swagger.js
+++ config/swagger.js
@@ -15,7 +15,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: 'https://balcao-de-negocios.vercel.app',
         description: 'Servidor de Desenvolvimento'
       },
       {
